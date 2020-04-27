2db7048452c820bcb7991ffff41d9a565fe935ef
Ryan Skog <ryan.skog@outlook.com>
2020-04-26 20:52:58 -0700

    Date properties populated, schedule displays with color, other elements in place

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+// Create divs for each time slot (6am-11p)
+// Each div creates:
+//  -uneditable time declaration
+//  -editable text box to enter appts
+//  -save button
+// Each div reads current time, and color codes the text boxes accordingly
+//  -past time is gray
+//  -current time is red
+//  -future time is green
+// Current date should be displayed in currentDay <p>
+// When user refreshes the screen, all appointments should remain***********
